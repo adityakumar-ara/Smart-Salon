7c403c24af01d7887d08d1e6a744b6d2d1705de7
Aditya Kumar <adityaara7667@gmail.com>
2026-07-19 11:05:41 +0530

    update

--- haircutservice/staticfiles/js/home.js
+++
@@ -1,9 +0,0 @@
-
-const slider = document.querySelector('.custom-slider');
-
-if (slider) {
-    slider.addEventListener('wheel', (evt) => {
-        evt.preventDefault();
-        slider.scrollLeft += evt.deltaY * 1.2;
-    }, { passive: false }); 
-}
