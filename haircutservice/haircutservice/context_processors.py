3fc005b042fbbc324830064ff3e0c2e067838de5
Aditya Kumar <adityaara7667@gmail.com>
2026-08-01 10:41:20 +0530

    add more

---
+++ haircutservice/haircutservice/context_processors.py
@@ -0,0 +1,8 @@
+from shopkeeper.models import Salon
+
+
+def has_salon_context_processor(request):
+    has_salon = False
+    if request.user.is_authenticated:
+        has_salon = Salon.objects.filter(owner=request.user).exists()
+    return {'has_salon': has_salon}
