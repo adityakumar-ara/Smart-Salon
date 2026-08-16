3adc32382fd8050b5ff732822d0622fc20800255
Aditya Kumar <adityaara7667@gmail.com>
2026-08-16 22:45:19 +0530

    add more

--- haircutservice/haircutservice/settings.py
+++ haircutservice/haircutservice/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts',
     'customer',
     'shopkeeper',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,17 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'supportsalon@gmail.com')
+
+
+
+#for upload media on CLOUDINARY
+# settings.py
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
